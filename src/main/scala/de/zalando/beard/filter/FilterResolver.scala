3405e2a8db8d77a033bad1b80cd33b4b3334a259
Boopathi Rajaa <me@boopathi.in>
2016-03-13 23:21:14 +0100

    Simple Mutable State plugin system

--- src/main/scala/de/zalando/beard/filter/FilterResolver.scala
+++ src/main/scala/de/zalando/beard/filter/FilterResolver.scala
@@ -1,39 +1,36 @@
 package de.zalando.beard.filter
 
 import org.slf4j.LoggerFactory
-
-import de.zalando.beard.ast.Identifier
-import scala.collection.immutable.{Set, Seq, Map}
+import scala.collection.immutable.Map
 
 /**
   * @author dpersa
   */
 trait FilterResolver {
+  var registry: Map[String, Filter]
+  def resolve(identifier: String, parameterNames: Set[String]): Option[Filter]
+  def register(filter: Filter)
+  def apply() = this
+}
 
-  def registeredFilters = Seq(
+object DefaultFilterResolver extends FilterResolver {
+  val logger = LoggerFactory.getLogger(this.getClass)
+
+  val defaultFilters = Seq(
     LowercaseFilter(),
     UppercaseFilter(),
     DateFormatFilter(),
     CapitalizeFilter()
   )
 
-  def filters: Map[String, Filter]
+  var registry = defaultFilters.map(filter => (filter.name, filter)).toMap[String, Filter]
 
-  def resolve(identifier: String, parameterNames: Set[String]): Option[Filter]
-}
-
-case class DefaultFilterResolver() extends FilterResolver {
-  val logger = LoggerFactory.getLogger(this.getClass)
-  
   override def resolve(identifier: String, parameterNames: Set[String]): Option[Filter] = {
     logger.info(s"Resolve filter ${identifier}")
-    
-    filters.get(identifier)
+    registry.get(identifier)
   }
 
-  override def filters: Map[String, Filter] = {
-    registeredFilters.map { case filter =>
-      (filter.name, filter)
-    }.toList.toMap[String, Filter]
+  override def register(filter: Filter) = {
+    registry = registry + (filter.name -> filter)
   }
 }
